3c4a8d034bdd0ffb26297e363b9ddfaae133a12f
trueartistee <75436229+trueartistee@users.noreply.github.com>
2023-12-11 21:22:02 +0100

    Add files via upload

---
+++ apply.js
@@ -0,0 +1,8 @@
+let btn1 = document.getElementsByClassName("click");
+let para1 = document.getElementById("first");
+let para2 = document.getElementById("second");
+function apply(){
+    para1.style.visibility ="visible";
+    para2.style.visibility ="visible";
+    btn1.style.visibility ="hidden";
+}
